9aabae8dd0d73649ba12963e0ff7df48b0d79d40
Gordy <bpm@plecs.org>
2026-01-09 17:04:25 -0500

    fix file reading

--- defaults.pl
+++
@@ -1,6 +0,0 @@
-
-db_username="dcs_commandstaff";
-db_password="dcs_commandstaff_password";
-db_name="dcs_commandstaff";
-
-
--- library.pl
+++ library.pl
@@ -2,24 +2,28 @@ sub read_configuration_file {
 	# initialize configuration array
 	my @config = (); 
 
-	$config_file = "defaults";
+	$config_file = "./defaults";
 
 	# open file N
  	open(my $config_fh, "<", $config_file) or die "Could not open file '$filename' $!";
 
 	# read system default
-	while (my $config_line = $config_fh) { 
+	while (my $config_line = <$config_fh>) { 
+
+		#print $config_line;
 		# strip newline
-		chomp($config_line); 
+		#		chomp($config_line); 
 		# read equals-separated label and value pair
-		($label, $value) = split(/=/,$config_line);
+		@configdata = split(/:/,$config_line);
 		# add label and value pair to the config array
-		push @config, [ $label, $value ]; 
+		#		push @config, [ $configdata[0], $configdata[1] ]; 
+		printf("%s %s\n", @configdata[0], @configdata[1]); 
 
 		# BUG - should replace rows that match the same label, maybe?
 
 		}
 	#
+	close($config_fh);
 	
 	
 	
